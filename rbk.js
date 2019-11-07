b3493b1e529e753d6d537b218457a927ebd048bc
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-07 16:51:16 +0100

    ideas4

--- rbk.js
+++ rbk.js
@@ -1,6 +1,11 @@
 $('#oneWin').hide()
 $('#twoWin').hide()
+$('#inputName1').hide()
+$('#inputName2').hide()
+$('label').hide()
+$('#multiplayer').on('click',function() {
+	$('#inputName1').show()
+	$('#inputName2').show()
+	$('label').show()
+})
 
-function game() {
-
-}
