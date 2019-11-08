d9091d62420572beb5b45152b28d7dabf99ab156
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-08 12:22:49 +0100

    16

--- TIC TAK TOE.js
+++ TIC TAK TOE.js
@@ -1,61 +1,75 @@
 function one(){
-$('#game').hide()
-$('#panel').hide()
-$('.startUp').hide()
-$('.popUpBtn').hide()
-$('h5').hide()
-$('.about').hide()
-$('.startUp').slideDown(1000)
-$('.popUpBtn').fadeIn(2000)
+$('#game').hide();
+$('#panel').hide();
+$('.startUp').hide();
+$('.popUpBtn').hide();
+$('h5').hide();
+$('.about').hide();
+$('.startUp').slideDown(1000);
+$('.popUpBtn').fadeIn(2000);
 $('h5').fadeIn(3000)
-}
+};
 one()
 
 $('#start').on('click', function() {
 
-$('.startUp').slideUp(1000)
-$('#game').slideDown(3000)
-$('#panel').slideDown(3000)
+$('.startUp').slideUp(1000);
+$('#game').slideDown(2000);
+$('#panel').slideDown(2000);
 
 });
 
 $('#about').on('click', function() {
 
-$('.startUp').slideUp(1000)
-$('.about').slideDown(3000)
+$('.startUp').slideUp(1000);
+$('.about').slideDown(3000);
 
 });
 
 $('#back').on('click', function() {
 
-$('.about').slideUp(1000)
-$('.startUp').slideDown(3000)
+$('.about').slideUp(1000);
+$('.startUp').slideDown(3000);
 
 });
 
+$('#mainMenu').click(function() {
 
-var player1Name="" 
-var player2Name=""
+$('#game').fadeOut(1000);
+$('#panel').fadeOut(1000);
+
+$('.startUp').fadeIn(2000);
+})
+
+
+var player1Name = ""; 
+var player2Name = "";
 var turn = "";
 var grid =  [[0,0,0],[0,0,0],[0,0,0]];
-var winner = 0
-var count=0;
+var winner = 0;
+var count = 0;
 
 
-function setTurn(){
+function setTurn() {
+
 	var random = Math.floor((Math.random() * 2) + 1);
-	hasWinner=0;
-	if(random == 1){
+
+	winner = 0;
+
+	if(random == 1) {
+
 		turn = player1Name;
 		msg(player1Name+"'s turn now!");
-	}
-	else{
+
+	}else {
+
 		turn = player2Name;
 		msg(player2Name+"'s turn now!");
+
 	}
 }
 
-function msg(x){
+function msg(x) {
 	return $("#board").text(x);
 }
 
@@ -65,62 +79,73 @@ function clear(){
 		msg("");
 		$(".tile").map(function() {
     		$(this).text("");
-		}).get();
+		});
+
 		winner = 0;
 		count = 0;
 }
 
-$("#playButton").click(function (){
+$("#playButton").click(function() {
 
-	if(winner==1){
+	if(winner === 1) {
+
 		clear();
 	}
 
 	player1Name = $("#player1").val();
 	player2Name = $("#player2").val();
 
-	if(player1Name=="" || player2Name==""){
+	if(player1Name === "" || player2Name === "") {
+
 		alert("Please set player all the names.");
+
 		return;
 	}
 
 	setTurn();
+
 });
 
-$(".tile").click(function (){
+$(".tile").click(function() {
 
-	if(player1Name=="" || player2Name==""){
+	if(player1Name=="" || player2Name=="") {
+
 		alert("Please set player all the names.");
+
 		return;
 	}
 
 	var row = $(this).parent().index();
 	var column = $(this).index();
 
-	if(grid[row][column]!==0){
+	if(grid[row][column] !== 0) {
+
 		alert("This position is taken. Please try other position.");
+
 		return;
 	}
-	if(winner==1){
+	if(winner === 1) {
+
 		alert("Please click play again");
+
 		return;
 	}
 
-	if(turn==player1Name){
+	if(turn==player1Name) {
 		count++;
 		$(this).text("O");
 		grid[row][column] = 1;
 		var Won = winnerCheck(1,player1Name);
 		if(!Won){
-			if(count>=9){
+			if(count >= 9){
 				msg("Match Drawn!");
 				count=0;
 				$("#playButton").text("Play again");
-				winner=1;
+				winner = 1;
 				return;
 			}else{
 				turn = player2Name;
-				msg(player2Name+"'s turn now!");
+				msg(player2Name + "'s turn now!");
 			}
 			return;	
 		}
@@ -129,21 +154,22 @@ $(".tile").click(function (){
 		}
 		
 	}
-	else if(turn==player2Name){
+	else if(turn === player2Name){
 		count++;
 		$(this).text("X");
 		grid[row][column] = 2;
 		var Won = winnerCheck(2,player2Name);
-		if(!Won){
-			if(count>=9){
+
+		if(!Won) {
+			if(count >= 9) {
 				msg("Match Drawn!");
-				count=0;
+				count = 0;
 				$("#playButton").text("Play again");
 				winner = 1;
 				return;
 			}else{
 				turn = player1Name;
-				msg(player1Name+"'s turn now!");
+				msg(player1Name + "'s turn now!");
 			}
 			return;	
 		}
@@ -166,7 +192,7 @@ function winnerCheck(n,playerName){
 		(grid[0][1]==n && grid[1][1]==n && grid[2][1]==n) ||
 		(grid[0][2]==n && grid[1][2]==n && grid[2][2]==n) ||
 
-		(grid[0][0]==n && grid[1][1]==n && grid[2][2]==n)||
+		(grid[0][0]==n && grid[1][1]==n && grid[2][2]==n)	||
 		(grid[0][2]==n && grid[1][1]==n && grid[2][0]==n)
 
 
